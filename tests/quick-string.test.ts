a286229961b8668dc0569de5a1c78143fb3358d2
Julien Capellari <julien.capellari@gmail.com>
2023-09-27 23:49:51 +0200

    fix: do not use alias in tests

--- tests/quick-string.test.ts
+++ tests/quick-string.test.ts
@@ -1,18 +1,18 @@
-import { qstr } from '@/src/alias.js';
+import { quickString } from '@/src/quick-string.js';
 
 // Tests
-describe('qstr', () => {
+describe('quickString', () => {
   it('should inject args into string as with classic templates', () => {
-    expect(qstr`life=${42}`).toBe('life=42');
+    expect(quickString`life=${42}`).toBe('life=42');
   });
 
   describe('quick conditions', () => {
     it('should inject text between #? and ?# has given value is truthy', () => {
-      expect(qstr`test #?:${true}is so cool that it ?#is successful`).toBe('test is so cool that it is successful');
+      expect(quickString`test #?:${true}is so cool that it ?#is successful`).toBe('test is so cool that it is successful');
     });
 
     it('should inject text between #? and ?# has given value is falsy', () => {
-      expect(qstr`test #?:${false}is so cool that it ?#is successful`).toBe('test is successful');
+      expect(quickString`test #?:${false}is so cool that it ?#is successful`).toBe('test is successful');
     });
   });
 });
