7f3dc5e7d3d5ba0fac95b03bd9f7c7bf711045ef
Julien Capellari <julien.capellari@gmail.com>
2023-09-30 23:35:02 +0200

    fix: global exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export * from './commands/index.js';
 export * from './global.js';
 export * from './quick.js';
+export * from './quick-wrapper.js';
 export * from './tree/index.js';
+export * from './types.js';
