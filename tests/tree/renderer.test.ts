c86ae5d9d3f705995f748297a432b79991fcd8c8
Julien Capellari <julien.capellari@gmail.com>
2023-09-30 12:59:47 +0200

    chore: test renderer

---
+++ tests/tree/renderer.test.ts
@@ -0,0 +1,124 @@
+import { renderQuickNodes } from '@/src/tree/renderer.js';
+import { QuickCommand } from '@/src/types.js';
+import { QuickRootNode } from '@/src/tree/index.js';
+
+// Setup
+let commands: Map<string, QuickCommand>;
+
+beforeEach(() => {
+  commands = new Map();
+});
+
+// Tests
+describe('renderQuickNodes', () => {
+  it('should concatenates text nodes with arg nodes into a string', () => {
+    const root: QuickRootNode = { // qstr`a${1}b${2}c`
+      type: 'root',
+      children: [
+        { type: 'text', text: 'a' },
+        { type: 'arg', index: 0 },
+        { type: 'text', text: 'b' },
+        { type: 'arg', index: 1 },
+        { type: 'text', text: 'c' },
+      ]
+    };
+
+    expect(renderQuickNodes(root, [1, 2], commands))
+      .toBe('a1b2c');
+  });
+
+  describe('with a command', () => {
+    it('should call command format on argument and inject result in final string', () => {
+      const root: QuickRootNode = { // qstr`a${1}#!life:${2}c`
+        type: 'root',
+        children: [
+          { type: 'text', text: 'a' },
+          { type: 'arg', index: 0 },
+          {
+            type: 'command',
+            name: 'life',
+            arg: { type: 'arg', index: 1 },
+          },
+          { type: 'text', text: 'c' },
+        ]
+      };
+
+      const command: QuickCommand = {
+        name: 'life',
+        format: vi.fn(() => 'life'),
+      };
+
+      commands.set('life', command);
+
+      expect(renderQuickNodes(root, [1, 42], commands))
+        .toBe('a1lifec');
+
+      expect(command.format).toHaveBeenCalledWith(42);
+    });
+
+    it('should ignore unknown command', () => {
+      const root: QuickRootNode = { // qstr`a${1}#!life:${2}c`
+        type: 'root',
+        children: [
+          { type: 'text', text: 'a' },
+          { type: 'arg', index: 0 },
+          {
+            type: 'command',
+            name: 'life',
+            arg: { type: 'arg', index: 1 },
+          },
+          { type: 'text', text: 'c' },
+        ]
+      };
+
+      expect(renderQuickNodes(root, [1, 42], commands))
+        .toBe('a1c');
+    });
+  });
+
+  describe('with a condition', () => {
+    it('should render inside of the condition as its value is true', () => {
+      const root: QuickRootNode = { // qstr`a${1}#?:${true}b?#${2}c`
+        type: 'root',
+        children: [
+          { type: 'text', text: 'a' },
+          { type: 'arg', index: 0 },
+          {
+            type: 'condition',
+            value: { type: 'arg', index: 1 },
+            children: [
+              { type: 'text', text: 'b' },
+            ]
+          },
+          { type: 'arg', index: 2 },
+          { type: 'text', text: 'c' },
+        ]
+      };
+
+      expect(renderQuickNodes(root, [1, true, 2], commands))
+        .toBe('a1b2c');
+    });
+
+    it('should not render inside of the condition as its value is false', () => {
+      const root: QuickRootNode = { // qstr`a${1}#?:${false}b?#${2}c`
+        type: 'root',
+        children: [
+          { type: 'text', text: 'a' },
+          { type: 'arg', index: 0 },
+          {
+            type: 'condition',
+            value: { type: 'arg', index: 1 },
+            children: [
+              { type: 'text', text: 'b' },
+            ]
+          },
+          { type: 'arg', index: 2 },
+          { type: 'text', text: 'c' },
+        ]
+      };
+
+      expect(renderQuickNodes(root, [1, false, 2], commands))
+        .toBe('a12c');
+    });
+  });
+});
