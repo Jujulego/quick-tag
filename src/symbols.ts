222f8b1e60e2b9a686dfdfa7a4ffffa2c2d33baf
Julien Capellari <julien.capellari@gmail.com>
2024-04-09 12:57:01 +0200

    fix: symbol in duplicated dep cases

--- src/symbols.ts
+++ src/symbols.ts
@@ -1,3 +1,3 @@
 // Constants
-export const QUICK_ARG_INJECTOR = Symbol('jujulego/quick-tag:arg-injector');
-export const QUICK_CONDITION_INJECTOR = Symbol('jujulego/quick-tag:condition-injector');
+export const QUICK_ARG_INJECTOR = Symbol.for('jujulego/quick-tag:arg-injector');
+export const QUICK_CONDITION_INJECTOR = Symbol.for('jujulego/quick-tag:condition-injector');
